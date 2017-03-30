c9d7edffc0d30beb312cf635544f66cc75763ee5
mczal <ijalcavaliers@yahoo.co.id>
2017-03-30 14:08:39 +0700

    debugging to rtf on lab

--- src/main/java/com/mczal/nb/SpringNaivebayesApplication.java
+++ src/main/java/com/mczal/nb/SpringNaivebayesApplication.java
@@ -4,20 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 //@PropertySource("file:${NAIVE_CONF_DIR}/application.properties")
 //@PropertySource(value = "classpath:${NAIVE_CONF_DIR}/application.properties")
 //@PropertySource(value = "classpath*:application.properties")
-public class SpringNaivebayesApplication extends SpringBootServletInitializer {
+public class SpringNaivebayesApplication
+    extends SpringBootServletInitializer {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringNaivebayesApplication.class, args);
+  }
 
   @Override
   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
     return application.sources(SpringNaivebayesApplication.class);
-  }
-
-  public static void main(String[] args) {
-    SpringApplication.run(SpringNaivebayesApplication.class, args);
   }
 }
