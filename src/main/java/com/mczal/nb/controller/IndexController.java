bbae670a81ad45f7a1854fb93c74540f37bda69e
Fahrizal <mczal@Fahrizals-MacBook-Pro.local>
2017-07-14 11:26:46 +0700

    done apply pdf show

--- src/main/java/com/mczal/nb/controller/IndexController.java
+++ src/main/java/com/mczal/nb/controller/IndexController.java
@@ -1,7 +1,16 @@
 package com.mczal.nb.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  * Created by Gl552 on 2/11/2017.
@@ -9,8 +18,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-  @RequestMapping({"","/"})
-  public String index(){
+  @Value("${hadoop.help.guides.pdf}")
+  private String helpGuidesPdf;
+
+  @RequestMapping(value = "/guides",
+      method = RequestMethod.GET)
+  public ResponseEntity<byte[]> getPDF1() throws Exception {
+
+    HttpHeaders headers = new HttpHeaders();
+
+    headers.setContentType(MediaType.parseMediaType("application/pdf"));
+    String filename = "help.pdf";
+
+    byte[] bFile = Files.readAllBytes(Paths.get(helpGuidesPdf));
+
+    headers.add("content-disposition", "inline;filename=" + filename);
+
+    headers.setContentDispositionFormData(filename, filename);
+    headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
+    ResponseEntity<byte[]> response = new ResponseEntity<byte[]>(bFile, headers, HttpStatus.OK);
+    return response;
+  }
+
+  @RequestMapping({"", "/"})
+  public String index() {
     return "redirect:/admin/home";
   }
 
